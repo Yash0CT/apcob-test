dfc4fe47733523dabf6542b70d63ba139df8e46a
Yash0CT <bharanikapulayaswanth.codetree@gmail.com>
2026-07-22 10:50:27 +0530

    registration form control

--- src/app/components/registration/registration.ts
+++ src/app/components/registration/registration.ts
@@ -1,37 +1,38 @@
 import { Component, inject, signal } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { Router, RouterLink } from '@angular/router';
 import { UserData } from '../../service/user-data';
 
 @Component({
   selector: 'app-registration',
-  imports: [FormsModule,RouterLink],
+  imports: [ReactiveFormsModule,RouterLink],
   templateUrl: './registration.html',
   styleUrl: './registration.css',
 })
 export class Registration {
+  public fb = inject(FormBuilder)
   private router = inject(Router)
   private userDataService = inject(UserData)
 
-  registerData = {
-    username: '',
-    email: '',
-    password:'',
-    confirmPassword:''
-  }
+  registrationForm : FormGroup = this.fb.group({
+    name : [''],
+    email: [''],
+    city: [''],
+    username: [''],
+    password: [''],
+    confirmPassword: ['']
+  })
 
   errorMessage = signal< string | null >(null);
 
   onRegister(){
     this.errorMessage.set(null);
-
-    if (this.registerData.confirmPassword !== this.registerData.password) {
-      this.errorMessage.set("Passwords doesn't match");
-      return;
+    if(this.registrationForm.value.password !==this.registrationForm.value.confirmPassword){
+      this.errorMessage.set("Passwords do not match")
     }
 
     // Hand data over to service
-    const result = this.userDataService.registerUser(this.registerData);
+    const result = this.userDataService.registerUser(this.registrationForm.value);
 
     if (result.success) {
       alert('Registration successful! (' + result.message + ')');
